0281881e553fc6bb82c827d3a25229372f6d3115
Adam Bissonnette <info@mediamanifesto.com>
2015-04-07 19:13:58 -0600

    Added rudimentary angular form

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Scavenger Backend </title>
+        <script src='https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js'></script>
+    </head>
+    <body ng-app="scavengerApp" ng-controller="scavengerCtrl">
+        <form>
+            <div class="input">
+                <input type="text" ng-model="formData.value" />
+            </div>
+            <button ng-click="formData.submit()" type="submit">Add Clue</button>
+        </form>
 
-require_once "bootstrap.php";
-
-$clueRepo = $entityManager->getRepository('Answer');
-$clues = $clueRepo->findAll();
-
-foreach ($clues as $clue) {
-    echo $clue->toString() . "\n" ;
-}
+<pre>
+{{formData}}
+</pre>
+        <script src='js/scavengerCtrl.js'></script>
+    </body>
+</html>
---
+++ js/scavengerCtrl.js
@@ -0,0 +1,20 @@
+angular.module('scavengerApp', [])
+    .controller('scavengerCtrl', ['$scope', '$http', function($scope, $http) {
+        $scope.formData = {"value" : ""};
+
+
+        $scope.formData.submit = function(item, event) {
+           console.log("--> Submitting form");
+           var dataObject = $scope.formData;
+
+           var responsePromise = $http.post("index.php", dataObject, {});
+           responsePromise.success(function(dataFromServer, status, headers, config) {
+              console.log(dataFromServer.title);
+           });
+            responsePromise.error(function(data, status, headers, config) {
+              alert("Submitting form failed!");
+           });
+         }
+
+    }]);
+
